7b8283867b6c8e14e47a28c0491be9c8e05e1f8d
Aemond <45364616+underpaks@users.noreply.github.com>
2025-03-27 07:47:00 -0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'BF98B4A18A1D173E626F95644FA3A'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///sgs.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
